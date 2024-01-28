896b5d4f04e460ae7b01bc15a7414929ccf8c95c
lucien <gnepadul@icloud.com>
2024-01-28 08:56:51 +0800

    Update snell_ipv6.sh

--- snell_ipv6.sh
+++ snell_ipv6.sh
@@ -3,48 +3,46 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 CONF="/etc/snell/snell-server.conf"
 SYSTEMD="/etc/systemd/system/snell.service"
-apt-get install unzip -y
+apt install unzip -y
 cd ~/
-wget --no-check-certificate -O snell.zip https://github.com/nobut/snell/raw/ec0772231970764182563f51fe255d1b2c881ba4/snell-server-v4.0.1-linux-amd64.zip
+wget --no-check-certificate -O snell.zip https://dl.nssurge.com/snell/snell-server-v4.0.1-linux-amd64.zip
 unzip -o snell.zip
 rm -f snell.zip
 chmod +x snell-server
 mv -f snell-server /usr/local/bin/
-if [ -f ${CONF} ]; then
-  echo "Found existing config..."
-  else
-  if [ -z ${PSK} ]; then
-    PSK=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 16)
-    echo "Using generated PSK: ${PSK}"
-  else
-    echo "Using predefined PSK: ${PSK}"
-  fi
-  mkdir /etc/snell/
-  echo "Generating new config..."
-  echo "[snell-server]" >>${CONF}
-  echo "listen = :::1024" >>${CONF}
-  echo "ipv6 = true" >>${CONF}
-  echo "psk = ${PSK}" >>${CONF}
-  echo "obfs = http" >>${CONF}
-fi
-if [ -f ${SYSTEMD} ]; then
-  echo "Found existing service..."
-  systemctl daemon-reload
-  systemctl restart snell
+if [ ! -f ${CONF} ]; then
+    mkdir -p /etc/snell/
+    if [ -z ${PSK} ]; then
+        PSK=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 16)
+        echo "Generated PSK: ${PSK}"
+    else
+        echo "Using predefined PSK: ${PSK}"
+    fi
+    echo "Generating new config..."
+    echo "[snell-server]" > ${CONF}
+    echo "listen = :::1024" >> ${CONF}
+    echo "ipv6 = true" >> ${CONF}
+    echo "psk = ${PSK}" >> ${CONF}
+    echo "obfs = http" >> ${CONF}
 else
-  echo "Generating new service..."
-  echo "[Unit]" >>${SYSTEMD}
-  echo "Description=Snell Proxy Service" >>${SYSTEMD}
-  echo "After=network.target" >>${SYSTEMD}
-  echo "" >>${SYSTEMD}
-  echo "[Service]" >>${SYSTEMD}
-  echo "Type=simple" >>${SYSTEMD}
-  echo "LimitNOFILE=32768" >>${SYSTEMD}
-  echo "ExecStart=/usr/local/bin/snell-server -c /etc/snell/snell-server.conf" >>${SYSTEMD}
-  echo "" >>${SYSTEMD}
-  echo "[Install]" >>${SYSTEMD}
-  echo "WantedBy=multi-user.target" >>${SYSTEMD}
-  systemctl daemon-reload
-  systemctl enable snell
-  systemctl start snell
+    echo "Found existing config..."
+fi
+if [ ! -f ${SYSTEMD} ]; then
+    echo "Generating new service..."
+    echo "[Unit]" > ${SYSTEMD}
+    echo "Description=Snell Proxy Service" >> ${SYSTEMD}
+    echo "After=network.target" >> ${SYSTEMD}
+    echo "[Service]" >> ${SYSTEMD}
+    echo "Type=simple" >> ${SYSTEMD}
+    echo "LimitNOFILE=32768" >> ${SYSTEMD}
+    echo "ExecStart=/usr/local/bin/snell-server -c /etc/snell/snell-server.conf" >> ${SYSTEMD}
+    echo "[Install]" >> ${SYSTEMD}
+    echo "WantedBy=multi-user.target" >> ${SYSTEMD}
+    systemctl daemon-reload
+    systemctl enable snell
+    systemctl start snell
+else
+    echo "Found existing service..."
+    systemctl daemon-reload
+    systemctl restart snell
 fi
